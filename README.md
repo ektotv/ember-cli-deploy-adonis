0489b6f7983bef72c0e0def7739f73d574aa8e54
Liam Potter <liam@liampotter.co.uk>
2024-01-11 22:58:00 +0000

    Addon ready

--- README.md
+++ README.md
@@ -1,31 +1,93 @@
-# ember-cli-deploy-adonis-plugin
+# ember-cli-deploy-adonis
 
-[Short description of the addon.]
+This plugin is an ember-cli-deploy plugin that copies your application assets and index.html to your directory of choice.
 
-
-## Compatibility
-
-* Ember.js v4.4 or above
-* Ember CLI v4.4 or above
-* Node.js v16 or above
-
+Intended to be used with [AdonisJS](https://adonisjs.com/) but can be used where copying your project files to a directory is required.
 
 ## Installation
 
 ```
-ember install ember-cli-deploy-adonis-plugin
+pnpm add -D ember-cli-deploy-adonis
 ```
-
 
 ## Usage
 
-[Longer description of how to use the addon in apps.]
+Add an adonis section to your deploy config ENV:
 
+```javascript
+// config/deploy.js
+const ENV = {
+  build: {},
+  adonis: {
+    destDir: "../server/public/web/",
+    indexFileLocation: "../server/resources/views/ember-web-index.edge",
+    filePattern: [
+      "**/*.{js,css,png,gif,ico,jpg,webp,mp3,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,wasm,json}",
+      "!**/ember-electron/*",
+    ],
+  },
+};
+```
+
+In `ember-cli-build.js` update your fingerprinting options to include the `prepend` option:
+
+```javascript
+function isProduction() {
+  return EmberApp.env() === "production";
+}
+
+function getPublicAssetsUrl() {
+  if (isProduction()) {
+    if (process.env.ADONIS_BUILD) return "/web/";
+    return "https://cdn.ekto.tv/ekto-web/";
+  }
+
+  return "/";
+}
+
+const app = new EmberApp(defaults, {
+  fingerprint: {
+    prepend: getPublicAssetsUrl(),
+  },
+  // ... other options
+});
+```
+
+If you're using embroider set your `publicAssetsUrl`
+
+```javascript
+// ... other options
+packagerOptions: {
+  publicAssetURL: getPublicAssetsUrl(),
+  // ... other options
+},
+// ... other options
+```
+
+## AdonisJS
+
+Add a wildcard route to your adonis application
+
+```javascript
+Route.get("*", "EmberWebController.index");
+```
+
+Create a controller to serve your index file
+
+```javascript
+// app/Controllers/Http/EmberWebController.js
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+export default class EmberWebController {
+  public async index({ view }: HttpContextContract) {
+    return view.render('ember-web-index');
+  }
+}
+```
 
 ## Contributing
 
 See the [Contributing](CONTRIBUTING.md) guide for details.
-
 
 ## License
 
--- index.js
+++ index.js
@@ -1,5 +1,105 @@
+/* eslint-disable ember/avoid-leaking-state-in-ember-objects */
 'use strict';
+const BasePlugin = require('ember-cli-deploy-plugin');
+const copy = require('recursive-copy');
 
 module.exports = {
   name: require('./package').name,
+
+  createDeployPlugin: function (options) {
+    const DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      defaultConfig: {
+        distDir: (context) => context.distDir,
+        overwrite: true,
+        confirm: true,
+        filePattern: undefined,
+
+        didDeployMessage: function (context) {
+          const revisionKey =
+            context.revisionData && context.revisionData.revisionKey;
+          if (revisionKey) {
+            return '✅ Copied revision ' + revisionKey + '.';
+          }
+        },
+      },
+
+      requiredConfig: ['destDir', 'indexFileLocation'],
+
+      configure: function (/* context */) {
+        this.log('Setting ADONIS_BUILD env variable to true');
+        process.env.ADONIS_BUILD = true;
+
+        this.log('Validating Configuration');
+        [
+          'distDir',
+          'overwrite',
+          'confirm',
+          'didDeployMessage',
+          'filePattern',
+        ].forEach(this.applyDefaultConfigProperty.bind(this));
+
+        this.log('✅ Config OK');
+      },
+
+      upload: function (/* context */) {
+        const distDir = this.readConfig('distDir');
+        const destDir = this.readConfig('destDir');
+        const indexFileLocation = this.readConfig('indexFileLocation');
+
+        const options = {
+          overwrite: this.readConfig('overwrite'),
+          confirm: this.readConfig('confirm'),
+          dot: true,
+        };
+
+        const allFilesOptions = {
+          ...options,
+        };
+
+        if (this.readConfig('filePattern') !== undefined) {
+          allFilesOptions.filter = this.readConfig('filePattern');
+        }
+
+        this.log('Copying files from ' + distDir + ' to ' + destDir);
+
+        const allFiles = new Promise(function (resolve, reject) {
+          copy(distDir, destDir, allFilesOptions, function (error, results) {
+            if (error) {
+              return reject(new Error('❌ Could not copy files' + error));
+            }
+
+            resolve(results);
+          });
+        });
+
+        const indexFiles = new Promise(function (resolve, reject) {
+          copy(
+            distDir + '/index.html',
+            indexFileLocation,
+            options,
+            function (error, results) {
+              if (error) {
+                return reject(new Error('❌ Could not copy files' + error));
+              }
+
+              resolve(results);
+            }
+          );
+        });
+
+        return Promise.all([allFiles, indexFiles]);
+      },
+
+      didDeploy: function (/* context */) {
+        const didDeployMessage = this.readConfig('didDeployMessage');
+        if (didDeployMessage) {
+          this.log(didDeployMessage);
+        }
+      },
+    });
+
+    return new DeployPlugin();
+  },
 };
